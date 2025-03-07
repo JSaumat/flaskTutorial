5bc3912c2b5dcef37edde49bae17c5013e4881e6
JSaumat <j_saumat@mail.fhsu.edu>
2025-03-07 16:45:50 -0700

    Corrected the schema.sql file, had extra code breaking it.

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -15,23 +15,3 @@ CREATE TABLE post (
   body TEXT NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
-Add the Python functions that will run these SQL commands to the db.py file:
-
-flaskr/db.py
-def init_db():
-    db = get_db()
-
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-
-
-@click.command('init-db')
-def init_db_command():
-    """Clear the existing data and create new tables."""
-    init_db()
-    click.echo('Initialized the database.')
-
-
-sqlite3.register_converter(
-    "timestamp", lambda v: datetime.fromisoformat(v.decode())
-)
